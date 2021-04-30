38ceaf02797bd20605116a68586053cf83b969ce
Gustavo Antunes Malheiros <gustavoamalheiros@gmail.com>
2021-04-29 21:46:25 -0300

    Update Estagiario.php

--- app/Models/Estagiario.php
+++ app/Models/Estagiario.php
@@ -3,10 +3,18 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
+use Config\Modules;
 
 class Estagiario extends Model
 {
 	protected $DBGroup              = 'default';
 	protected $table                = 'estagiarios';
 	protected $allowedFields        = ['nome', 'anoIngresso', 'minicurriculo', 'curso', 'semestre', 'usuarioId'];
+}
+
+class Empregador extends Model{
+	protected $DBGroup = 'default';
+	protected $table = 'empregador';
+	protected $primarykey = 'usuarioId';
+	protected $allowedFields = ['usuarioId', 'nomeEmpresa', 'enderecoEmpresa', 'nomePessoaContato', 'descricaoEmpresa', 'descricaoProdutos'];
 }
